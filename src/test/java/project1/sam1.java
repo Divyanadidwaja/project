8396e3ac4a8c10217076395a9f1b441169411fe9
divya <divyadwaja@gmail.com>
2024-03-10 08:33:38 +0530

    design is ready

---
+++ src/test/java/project1/sam1.java
@@ -0,0 +1,13 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class sam1
+{
+	@Test
+	public void demo1()
+	{
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/project1/sam2.java
@@ -0,0 +1,14 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class sam2
+{
+	@Test
+	public void demo1()
+	{
+		System.out.println("hi");
+	}
+
+}
+
